d3a96f1269506652e21ec05a4fdb5b9efd233de3
CorneliusCornbread <8294697+23Link89@users.noreply.github.com>
2019-10-12 17:08:31 -0500

    Physics scene
    Setup a physics scene that has a bunch of physics objects that use a job to move.

--- Assets/Scripts/TransformSystem.cs
+++ Assets/Scripts/TransformSystem.cs
@@ -8,23 +8,72 @@ using Unity.Physics;
 
 public class TransformSystem : JobComponentSystem
 {
- 
-    /* Component system implementation
+    /* Component System implementation
     [BurstCompile]
     protected override void OnUpdate()
     {
         
-        Entities.ForEach((ref TransformStruct data, ref RotationEulerXYZ euler, ref Translation pos) =>
+        Entities.ForEach((ref TransformStruct data, ref InputStruct input, ref PhysicsVelocity rb) =>
         {
-            euler.Value += data.rotX * Time.deltaTime;
+            float deltaTime = Time.deltaTime;
 
-            pos.Value.y = 4;
+            //euler.Value += data.rotX * deltaTime;
 
-            //scale.Value += Time.deltaTime;
+            //euler.Value.x += data.speed * input.mouseX * deltaTime;
+            //euler.Value.z += data.speed * input.mouseY * deltaTime;
+
+            if (input.shift)
+            {
+                float horiz = data.speed * input.horizontal * deltaTime * 500;
+                float vert = data.speed * input.vertical * deltaTime * 500;
+
+                rb.Linear.z = horiz;
+                rb.Linear.x = -vert;
+            }
+
+            else
+            {
+                float horiz = data.speed * input.horizontal * deltaTime * 100;
+                float vert = data.speed * input.vertical * deltaTime * 100;
+
+                rb.Linear.z = horiz;
+                rb.Linear.x = -vert;
+            }
+
+            if (input.alt == 0)
+                return;
+
+            const float max = 10;
+
+            if (rb.Linear.y < max || rb.Linear.y > -max)
+            {
+                float alt = data.speed * input.alt * deltaTime * 100;
+                if (alt > 0 && rb.Linear.y + alt > max)
+                {
+                    float target = max - rb.Linear.y;
+                    if (target < 0)
+                        return;
+                    else
+                        alt = target;
+
+                }
+
+                else if (alt < 0 && rb.Linear.y + alt < -max)
+                {
+                    float target = (-max - rb.Linear.y);
+                    if (target > 0)
+                        return;
+                    else
+                        alt = target;
+                }
+
+                alt = Mathf.Clamp(alt, -max - 1, max + 1);
+
+                rb.Linear.y += alt;
+            }
         });
     }*/
 
-    
     //Job Component System implementation
     [BurstCompile]
     private struct TransformJob : IJobForEach<TransformStruct, PhysicsVelocity, InputStruct>
@@ -56,19 +105,32 @@ public class TransformSystem : JobComponentSystem
                 rb.Linear.x = -vert;
             }
 
-            float max = 20;
+            if (input.alt == 0)
+                return;
 
-            Debug.Log(rb.Linear.y);
+            const float max = 10;
 
             if (rb.Linear.y < max || rb.Linear.y > -max)
             {
                 float alt = data.speed * input.alt * deltaTime * 100;
+                if (alt > 0 && rb.Linear.y + alt > max)
+                {
+                    float target = max - rb.Linear.y;
+                    if (target < 0)
+                        return;
+                    else
+                        alt = target;
 
-                if (rb.Linear.y + alt > max)
-                    alt = max - rb.Linear.y;
+                }
 
-                else if (rb.Linear.y + alt < -max)
-                    alt = -max - rb.Linear.y;
+                else if (alt < 0 && rb.Linear.y + alt < -max)
+                {
+                    float target = (-max - rb.Linear.y);
+                    if (target > 0)
+                        return;
+                    else
+                        alt = target;
+                }
 
                 alt = Mathf.Clamp(alt, -max - 1, max + 1);
 
@@ -77,6 +139,7 @@ public class TransformSystem : JobComponentSystem
         }
     }
 
+    [BurstCompile]
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         TransformJob job = new TransformJob { deltaTime = Time.deltaTime };
