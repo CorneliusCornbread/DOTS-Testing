ac465c76aa4297bf0830a4d118cb6f9f97805aae
CorneliusCornbread <8294697+23Link89@users.noreply.github.com>
2019-10-14 22:45:28 -0500

    Finna die
    Rotation really cannot be controlled when using a physics body

--- Assets/Scripts/PhysicsControllerSystem.cs
+++ Assets/Scripts/PhysicsControllerSystem.cs
@@ -8,47 +8,12 @@ using UnityEngine;
 
 public class PhysicsControllerSystem : ComponentSystem
 {
-    public struct MoveJob : IJobForEach<PhysicsVelocity, InputStruct, PhysicsControllerStruct>
+    [BurstCompile]
+    public struct MoveJob : IJobForEach<PhysicsVelocity, InputStruct, PhysicsControllerStruct, LocalToWorld, RotationEulerXYZ>
     {
         public float deltaTime;
 
-        public void Execute(ref PhysicsVelocity rb, ref InputStruct input, ref PhysicsControllerStruct playerData)
-        {
-            Vector3 targetVelocity = new Vector3();
-            float speed = 1000;
-            float gravity = 2;
-            float mass = 3;
-
-            float maxVelocityChange = 10;
-
-            targetVelocity.z = input.horizontal;
-            targetVelocity.x = -input.vertical;
-
-            //Debug.Log(targetVelocity.x);
-
-            // Calculate how fast we should be moving
-            //targetVelocity = transform.TransformDirection(targetVelocity); //change from local space to world space
-            targetVelocity *= speed * deltaTime;
-
-            // Apply a force that attempts to reach our target velocity
-            Vector3 velocity = rb.Linear;
-            Vector3 velocityChange = (targetVelocity - velocity);
-            //velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-            //velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-            velocityChange.y = -gravity * mass; //If we are't wall running or climbing a ladder apply gravity to the player
-
-            Debug.Log(velocityChange.x);
-
-            rb.Linear.x += velocityChange.x;
-            rb.Linear.y += velocityChange.y;
-            rb.Linear.z += velocityChange.z;
-        }
-    }
-
-    [BurstCompile]
-    protected override void OnUpdate()
-    {
-        Entities.ForEach((ref PhysicsVelocity rb, ref InputStruct input, ref PhysicsControllerStruct playerData, ref LocalToWorld toWorld, ref Rotation rot) =>
+        public void Execute(ref PhysicsVelocity rb, ref InputStruct input, ref PhysicsControllerStruct playerData, ref LocalToWorld toWorld, ref RotationEulerXYZ rot)
         {
             float3 targetVelocity = new float3();
             float speed = 1000;
@@ -66,7 +31,7 @@ public class PhysicsControllerSystem : ComponentSystem
             // Calculate how fast we should be moving
             //targetVelocity = transform.TransformDirection(targetVelocity); //change from local space to world space
             targetVelocity = Rotate(toWorld.Value, targetVelocity); //Change from local space to world space
-            targetVelocity *= speed * Time.deltaTime;
+            targetVelocity *= speed * deltaTime;
 
             // Apply a force that attempts to reach our target velocity
             float3 velocity = rb.Linear;
@@ -77,6 +42,8 @@ public class PhysicsControllerSystem : ComponentSystem
 
             rb.Linear += velocityChange;
 
+            rot.Value = new float3(0, 0, 0);
+            /*
             float baseSpd = 2;
             float maxSpd = 4;
             float mult = 5;
@@ -125,8 +92,46 @@ public class PhysicsControllerSystem : ComponentSystem
             {
                 rb.Angular.z = 0;
             }
+            */
 
             //rot.Value.value = new float4(0, 0, 0, rot.Value.value.w);
+        }
+    }
+
+    
+    [BurstCompile]
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref PhysicsVelocity rb, ref InputStruct input, ref PhysicsControllerStruct playerData, ref LocalToWorld toWorld, ref RotationEulerXYZ rot) =>
+        {
+            float3 targetVelocity = new float3();
+            float speed = 1000;
+            float gravity = 1;
+            float mass = 1;
+
+            float maxVelocityChange = 10;
+
+            targetVelocity.z = input.horizontal;
+            targetVelocity.x = -input.vertical;
+
+            //Debug.Log(rot.Value.value.w);
+
+
+            // Calculate how fast we should be moving
+            //targetVelocity = transform.TransformDirection(targetVelocity); //change from local space to world space
+            targetVelocity = Rotate(toWorld.Value, targetVelocity); //Change from local space to world space
+            targetVelocity *= speed * Time.deltaTime;
+
+            // Apply a force that attempts to reach our target velocity
+            float3 velocity = rb.Linear;
+            float3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = math.clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = math.clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = -gravity * mass; //If we are't wall running or climbing a ladder apply gravity to the player
+
+            rb.Linear += velocityChange;
+
+            rot.Value = new float3(0, 0, 0);
         });
     }
 
@@ -135,4 +140,14 @@ public class PhysicsControllerSystem : ComponentSystem
     {
         return (a.c0 * b.x + a.c1 * b.y + a.c2 * b.z).xyz;
     }
+
+    /*
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        MoveJob job = new MoveJob
+        {
+            deltaTime = Time.deltaTime
+        };
+        return job.Schedule(this, inputDeps);
+    }*/
 }
