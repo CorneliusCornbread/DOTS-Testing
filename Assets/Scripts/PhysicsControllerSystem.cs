126e38d8f19cdd2a994dc80126f14a0d8adf4625
CorneliusCornbread <22rosenj@elmbrookstudents.org>
2019-10-17 09:57:30 -0500

    IJobChunk works
    Actually got IJobChunk to work.

--- Assets/Scripts/PhysicsControllerSystem.cs
+++ Assets/Scripts/PhysicsControllerSystem.cs
@@ -12,7 +12,7 @@ public class PhysicsControllerSystem : JobComponentSystem
 {
     private EntityQuery q;
 
-    //[BurstCompile]
+    [BurstCompile]
     public struct MoveJob : IJobChunk
     {
         public float deltaTime;
@@ -25,117 +25,73 @@ public class PhysicsControllerSystem : JobComponentSystem
 
         public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
         {
-            try
+            //Get arrays of objects that we'll be operating on
+            NativeArray<PhysicsVelocity> physVel = chunk.GetNativeArray(physVType);
+            NativeArray<InputStruct> inputStructs = chunk.GetNativeArray(inputType);
+            NativeArray<PhysicsControllerStruct> controllers = chunk.GetNativeArray(pControlType);
+            NativeArray<LocalToWorld> toWorlds = chunk.GetNativeArray(toWorldType);
+            NativeArray<Rotation> rotations = chunk.GetNativeArray(rotType);
+            NativeArray<Translation> translations = chunk.GetNativeArray(transType);
+
+            //Essentially IJobForEach
+            for (int i = 0; i < chunk.Count; i++)
             {
-                NativeArray<PhysicsVelocity> physVel = chunk.GetNativeArray(physVType);
-                NativeArray<InputStruct> inputStructs = chunk.GetNativeArray(inputType);
-                NativeArray<PhysicsControllerStruct> controllers = chunk.GetNativeArray(pControlType);
-                NativeArray<LocalToWorld> toWorlds = chunk.GetNativeArray(toWorldType);
-                NativeArray<Rotation> rotations = chunk.GetNativeArray(rotType);
-                NativeArray<Translation> translations = chunk.GetNativeArray(transType);
+                //Get data from arrays
+                PhysicsVelocity rb = physVel[i];
+                InputStruct inp = inputStructs[i];
+                PhysicsControllerStruct controllerStruct = controllers[i];
+                LocalToWorld toWorld = toWorlds[i];
+                Rotation rot = rotations[i];
+                Translation trans = translations[i];
 
-                for (int i = 0; i < chunk.Count; i++)
-                {
-                    //physVel[i] = new PhysicsVelocity();
-                    //inputStructs[i] = new InputStruct();
-                    //controllers[i] = new PhysicsControllerStruct();
-                    //toWorlds[i] = new LocalToWorld();
-                    //rotations[i] = new Rotation();
-                    //translations[i] = new Translation();
+                //Do work on data
+                Move(ref rb, ref inp, ref controllerStruct, ref toWorld, ref rot, ref trans);
 
-                    PhysicsVelocity rb = physVel[i];
-                    InputStruct inp = inputStructs[i];
-                    PhysicsControllerStruct controllerStruct = controllers[i];
-                    LocalToWorld toWorld = toWorlds[i];
-                    //Rotation rot = rotations[i];
-                    //Translation trans = translations[i];
-
-
-                    Execute(ref rb, ref inp, ref controllerStruct, ref toWorld);
-                }
-            }
-            catch (System.Exception e)
-            {
-
-                Debug.LogError(e);
+                //Set data in array to data we changed
+                physVel[i] = rb;
+                inputStructs[i] = inp;
+                controllers[i] = controllerStruct;
+                toWorlds[i] = toWorld;
+                rotations[i] = rot;
+                translations[i] = trans;
             }
         }
 
-        public void Execute(ref PhysicsVelocity rb, ref InputStruct input, ref PhysicsControllerStruct playerData, ref LocalToWorld toWorld)
+        public void Move(ref PhysicsVelocity rb, ref InputStruct input, ref PhysicsControllerStruct playerData, ref LocalToWorld toWorld, ref Rotation rot, ref Translation trans)
         {
-            Debug.Log("execute");
-
             float3 targetVelocity = new float3();
             float speed = 1000;
             float gravity = 1;
 
             float maxVelocityChange = 10;
 
+            //Set target to input velocity
             targetVelocity.z = input.horizontal;
             targetVelocity.x = -input.vertical;            
 
-            //Debug.Log(rot.Value.value.w);
-
-            // Calculate how fast we should be moving
-            //targetVelocity = transform.TransformDirection(targetVelocity); //change from local space to world space
-            targetVelocity = Rotate(toWorld.Value, targetVelocity); //Change from local space to world space
+            //Calculate how fast we should be moving
+            targetVelocity = TransformDirection(toWorld.Value, targetVelocity); //Change from local space to world space
             targetVelocity *= speed * deltaTime;
 
-            // Apply a force that attempts to reach our target velocity
+            //Apply a force that attempts to reach our target velocity
             float3 velocity = rb.Linear;
             float3 velocityChange = (targetVelocity - velocity);
             velocityChange.x = math.clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
             velocityChange.z = math.clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
             velocityChange.y = -gravity; //If we are't wall running or climbing a ladder apply gravity to the player
 
+            //Mouse movement
+            rb.Angular.y = input.mouseX * deltaTime * 100;
+
             rb.Linear += velocityChange;
-            Debug.Log("execute end");
         }
     }
 
-    /*
     [BurstCompile]
-    protected override void OnUpdate()
-    {
-        Entities.ForEach((ref PhysicsVelocity rb, ref InputStruct input, ref PhysicsControllerStruct playerData, ref LocalToWorld toWorld, ref RotationEulerXYZ rot) =>
-        {
-            float3 targetVelocity = new float3();
-            float speed = 1000;
-            float gravity = 1;
-            float mass = 1;
-
-            float maxVelocityChange = 10;
-
-            targetVelocity.z = input.horizontal;
-            targetVelocity.x = -input.vertical;
-
-            //Debug.Log(rot.Value.value.w);
-
-
-            // Calculate how fast we should be moving
-            //targetVelocity = transform.TransformDirection(targetVelocity); //change from local space to world space
-            targetVelocity = Rotate(toWorld.Value, targetVelocity); //Change from local space to world space
-            targetVelocity *= speed * Time.deltaTime;
-
-            // Apply a force that attempts to reach our target velocity
-            float3 velocity = rb.Linear;
-            float3 velocityChange = (targetVelocity - velocity);
-            velocityChange.x = math.clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-            velocityChange.z = math.clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-            velocityChange.y = -gravity * mass; //If we are't wall running or climbing a ladder apply gravity to the player
-
-            rb.Linear += velocityChange;
-
-            rot.Value = new float3(0, 0, 0);
-        });
-    }*/
-
-    [BurstCompile]
-    public static float3 Rotate(float4x4 a, float3 b)
+    public static float3 TransformDirection(float4x4 a, float3 b)
     {
         return (a.c0 * b.x + a.c1 * b.y + a.c2 * b.z).xyz;
     }
-
 
     protected override void OnCreate()
     {
@@ -152,6 +108,7 @@ public class PhysicsControllerSystem : JobComponentSystem
 
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
+        //Setup job
         MoveJob job = new MoveJob
         {
             deltaTime = Time.deltaTime,
