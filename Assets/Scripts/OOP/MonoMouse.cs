e47ec9f47be73a1bc7743f53ecaa34576ec71399
CorneliusCornbread <8294697+23Link89@users.noreply.github.com>
2019-11-15 20:14:41 -0600

    MonoMove stuff
    Created mono based movement, gonna try to get some hybrid ECS going

--- Assets/Scripts/OOP/MonoMouse.cs
+++ Assets/Scripts/OOP/MonoMouse.cs
@@ -1,32 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Unity.Burst;
 
-public class MonoMouse : MonoBehaviour
+namespace MonoScripts
 {
-    float vertRot;
-    public float sense = 2;
-
-    public Camera cam;
-    public GameObject visor;
-
-    void OnEnable()
+    [BurstCompile]
+    public class MonoMouse : MonoBehaviour
     {
-        if (cam == null || visor == null)
+        float vertRot;
+        public float sense = 2;
+
+        public Camera cam;
+        public GameObject visor;
+
+        void OnEnable()
         {
-            BurstDebug.LogWarning("Cam or visor net set in MonoMouse on object " + name + ", disabling");
-            enabled = false;
+            if (cam == null || visor == null)
+            {
+                BurstDebug.LogWarning("Cam or visor net set in MonoMouse on object " + name + ", disabling");
+                enabled = false;
+            }
         }
-    }
 
-    void Update()
-    {
-        float rotLeftRight = MonoInputManager.instance.mouse.x * sense;
-        transform.Rotate(0, rotLeftRight, 0);
+        void Update()
+        {
+            #if UNITY_EDITOR
+            if (MonoInputManager.instance == null)
+            {
+                BurstDebug.Log("Waiting for MonoInputManager");
+                return;
+            }
+            #endif
 
-        vertRot -= MonoInputManager.instance.mouse.y * sense;
-        vertRot = Mathf.Clamp(vertRot, -45, 45); //Clamps the camera so you can't turn into an owl and look all the way up and behind you
-        cam.transform.localRotation = Quaternion.Euler(vertRot, 0, 0);
-        visor.transform.localRotation = Quaternion.Euler(vertRot, 0, 0); // Moves the visor
+            float rotLeftRight = MonoInputManager.instance.mouse.x * sense;
+            transform.Rotate(0, rotLeftRight, 0);
+
+            vertRot -= MonoInputManager.instance.mouse.y * sense;
+            vertRot = Mathf.Clamp(vertRot, -45, 45); //Clamps the camera so you can't turn into an owl and look all the way up and behind you
+            cam.transform.localRotation = Quaternion.Euler(vertRot, 0, 0);
+            visor.transform.localRotation = Quaternion.Euler(vertRot, 0, 0); // Moves the visor
+        }
     }
 }
---
+++ Assets/Scripts/OOP/MonoMove.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace MonoScripts
+{
+    [BurstCompile]
+    public class MonoMove : MonoBehaviour
+    {
+        public Rigidbody rb;
+        public const float gravity = 4;
+        public const float speed = 16;
+        public const float maxVelocityChange = 30;
+
+        public bool IsGrounded { get; private set; } = false;
+
+        NativeArray<RaycastHit> raycastHits;
+        NativeArray<RaycastCommand> commands;
+        JobHandle raycastJob;
+
+        private void Start()
+        {
+            
+
+            if (rb == null)
+            {
+                BurstDebug.LogWarning("Rigidbody is not set on object " + name);
+                enabled = false;
+            }
+        }
+
+        public void Update()
+        {
+            commands[0] = new RaycastCommand(transform.position, Vector3.down, 1.25f);
+
+            raycastJob = RaycastCommand.ScheduleBatch(commands, raycastHits, 1);
+        }
+
+        public void LateUpdate()
+        {
+            raycastJob.Complete();
+
+            if (raycastHits[0].transform != null)
+            {
+                IsGrounded = true;
+            }
+
+            else
+            {
+                IsGrounded = false;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            #if UNITY_EDITOR
+            if (MonoInputManager.instance == null)
+            {
+                BurstDebug.Log("Waiting for MonoInputManager");
+                return;
+            }
+            #endif
+
+            float2 input = MonoInputManager.instance.smoothedMove;
+            float3 targetVelocity = new float3(input.x, 0, input.y);            
+
+            // Calculate how fast we should be moving
+            targetVelocity = transform.TransformDirection(targetVelocity); //change from local space to world space
+            targetVelocity *= speed;
+
+            // Apply a force that attempts to reach our target velocity
+            float3 velocity = rb.velocity;
+            float3 velocityChange = (targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = -gravity * rb.mass / 10; //Apply gravity
+
+            if (MonoInputManager.instance.jump && IsGrounded)
+            {
+                velocityChange.y = 0;
+                rb.velocity = new Vector3(rb.velocity.x, 26, rb.velocity.z);
+            }
+
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
+
+        public void OnDisable()
+        {
+            raycastHits.Dispose();
+            commands.Dispose();
+        }
+
+        private void OnEnable()
+        {
+            raycastHits = new NativeArray<RaycastHit>(1, Allocator.Persistent);
+            commands = new NativeArray<RaycastCommand>(1, Allocator.Persistent);
+        }
+    }
+}
